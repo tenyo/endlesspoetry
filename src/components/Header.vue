62265e7d37fa652c936ae63e83f557f306ce7994
Tenyo Grozev <tenyo.grozev@yale.edu>
2019-01-02 15:39:30 -0500

    fix favicon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar fixed-top navbar-dark bg-dark">
-    <span class="navbar-brand"><img src="@/assets/quill.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    <span class="navbar-brand"><img src="@/assets/images/quill.png" width="30" height="30" class="d-inline-block align-top" alt="">
       Endless Poetry
     </span>
     <span class="navbar-text small">Powered by <a href="https://github.com/thundercomb/poetrydb" target="_blank">PoetryDB</a></span>
